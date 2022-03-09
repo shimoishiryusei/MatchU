3d340ddf13f305ee6d83c8a1f5d2d1a3ba4cac4d
shimoishiryusei <shimoishiryusei@gmail.com>
2022-03-10 01:51:03 +0900

    made basis of the page

--- src/pages/landingpage/LandingPage.js
+++ src/pages/landingpage/LandingPage.js
@@ -5,6 +5,15 @@ export default function LandingPage() {
   return (
     <div className='land-container'>
         <button type="button" className="btn btn-primary">Primary</button>
+        <div className='eye_catch'>
+          <h1>Header with eye catch</h1>  
+        </div>
+        <div className='summary'>
+          <h1>概要</h1>
+        </div>
+        <div className='explanation'>
+          <h1>説明</h1>  
+        </div>
 
     </div>
   )
